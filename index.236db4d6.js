eb8edf86c56d73ca0e09460d6d38b8cc99de188c
AntoninaGraur <124353835+AntoninaGraur@users.noreply.github.com>
2023-05-05 18:19:12 +0000

    Deploying to gh-pages from @ AntoninaGraur/goit-js-hw-10@3452ad3a633b4dc68bae2c034f3dfe3e421422d2 🚀

---
+++ index.236db4d6.js
@@ -0,0 +1,2 @@
+console.log(123);console.log("https://restcountries.com/v3.1/all?fields=name,flags/name");
+//# sourceMappingURL=index.236db4d6.js.map
---
+++ index.9150e34f.js
@@ -0,0 +1,2 @@
+!function(){console.log(123);console.log("https://restcountries.com/v3.1/all?fields=name,flags/name")}();
+//# sourceMappingURL=index.9150e34f.js.map
