ec567befad8e61cb19a18caa31d931347615ec7f
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-05-06 12:22:10 +0200

    probe

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,5 @@
-console.log(123);
+
+const BASE_URL = '';
+console.log(BASE_URL);
 
 
-
-const BASE_URL = 'https://restcountries.com/v3.1/all?fields=name,flags';
-const END_POINT = '/name';
-const PARAMS = '';
-
-const url = BASE_URL + END_POINT + PARAMS;
-
-// const promise = fetch();
-console.log(url);
--- src/index.js
+++ src/index.js
@@ -1,31 +1,5 @@
 import './css/styles.css';
-// import debounce from 'lodash.debounce';
-// import Notiflix from 'notiflix';
-// import 
-// const DEBOUNCE_DELAY = 300;
 
-// const searchInput = document.querySelector('#search-box');
-// searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+const coutryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
-
-// function onSearch(event) {
-//   const searchQuery = event.target.value.trim();
-//   if (!searchQuery) {
-//     clearSearchResults();
-//     return;
-//   }
-//   fetchCountries(searchQuery)
-//     .then(countries => {
-//       if (countries.length > 10) {
-//        Notiflix.Report.info('Too many matches found. Please enter a more specific name.');
-//         clearSearchResults();
-//       } else if (countries.length >= 2 && countries.length <= 10) {
-//         renderCountryList(countries);
-//       } else if (countries.length === 1) {
-//         renderCountryCard(countries[0]);
-//       } else {
-//         Notiflix.Report.failure('Oops, there is no country with that name.');
-//         clearSearchResults();
-//       }
-//     });
-// }
